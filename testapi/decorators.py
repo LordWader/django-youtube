5d0d649751b9def5f3d60b7362737ddb952ddde7
nipr1218 <nikolai.proshin@netcracker.com>
2019-07-16 10:38:14 +0300

    Hot fix for decorator

--- testapi/decorators.py
+++ testapi/decorators.py
@@ -4,9 +4,8 @@ from django.http import HttpResponse
 
 
 def authorization(f):
-    def wrapped_f(request):
+    def wrapped_f(request, *args, **kwargs):
         if not request.headers.get("authorization"):
             return HttpResponse(json.dumps({"error": "Only authorized persons with token"}), status=500)
-        return f(request)
-
+        return f(request, *args, **kwargs)
     return wrapped_f
